46c7ccbfb1309511f1e149a9ea23243e110cf072
Abhijay <73731755+Abhijay360@users.noreply.github.com>
2025-10-15 23:23:22 -0400

    Update  wordle.py

--- wordle.py
+++ wordle.py
@@ -1,3 +1,5 @@
+import random
+
 def get_guess():
     while True:
         guess = input("What word is this?: ")
@@ -6,7 +8,7 @@ def get_guess():
         print("Please enter a 5-letter word.")
 
 def print_word(word):
-    print(' '.join(word))
+    print(' '.join(list(word)))
 
 def exact_match_compare(solution, guess):
     result = ""
@@ -18,15 +20,13 @@ def exact_match_compare(solution, guess):
     return result
 
 def one_turn(solution):
-    guess = get_guess()  # Ask for player's guess
-    print_word(guess)    # Display guess with spaces
-    match = exact_match_compare(solution, guess)  # Compare and get result string
-    print(match)         # Output result with emojis
+    guess = get_guess()
+    print_word(guess)
+    match = exact_match_compare(solution, guess)
+    print(match)
     if guess == solution:
         print("Congratulations!")
         exit()
-
-import random
 
 def make_solution():
     words = [
@@ -35,11 +35,13 @@ def make_solution():
     ]
     return random.choice(words)
 
-soln = make_solution()        # Pick a random solution word
-one_turn(soln)                # 1st attempt
-one_turn(soln)                # 2nd attempt
-one_turn(soln)                # 3rd attempt
-one_turn(soln)                # 4th attempt
-one_turn(soln)                # 5th attempt
-one_turn(soln)                # 6th attempt
-print(f'Word was "{soln}", better luck next time.')  # Shown if user didn’t win
+if __name__ == '__main__':
+    soln = make_solution()
+    one_turn(soln)
+    one_turn(soln)
+    one_turn(soln)
+    one_turn(soln)
+    one_turn(soln)
+    one_turn(soln)
+    print(f'Word was "{soln}", better luck next time.')
+
